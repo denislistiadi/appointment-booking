cb584ffcf40c0d4d81fbe938c0235f0480442e97
Denis Listiadi <denislistiadi24@gmail.com>
2024-03-17 14:09:20 +0700

    feat(landing): add skeleton loading

--- app/_components/doctor-list.tsx
+++ app/_components/doctor-list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useDoctorList } from "@/hooks/useDoctorList";
 import Image from "next/image";
 import React from "react";
@@ -37,33 +38,50 @@ function DoctorList() {
     <div className="mb-5 mx-auto max-w-screen-xl px-4 pb-8 sm:px-6 sm:pb-12 lg:px-8 lg:pb-16">
       <h2 className="font-bold text-xl pb-6">Popular Doctors</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-        {data.map((item: IDoctor) => (
-          <div key={item.id} className="border hover:border-primary transition-all ease-in-out cursor-pointer rounded-xl p-3">
-            <Image
-              src={item.attributes.image.data.attributes.url}
-              alt={item.attributes.Name}
-              width={500}
-              height={200}
-              className="h-[200px] w-full object-cover rounded-lg "
-            />
-            <div className="mt-3 items-baseline gap-1 flex flex-col">
-              <p className="text-[10px] bg-blue-100 p-1 rounded-full px-2 text-primary">
-                {item.attributes.categories.data[0]?.attributes.Name}
-              </p>
-              <h2 className="font-bold">{item.attributes.Name}</h2>
-              <p className="text-primary text-sm">
-                {item.attributes.Year_of_Experience}
-              </p>
-              <p className="text-gray-500 text-sm">{item.attributes.Address}</p>
-              <Button
-                variant="outline"
-                className="text-primary my-1.5 text-sm border-primary rounded-full hover:bg-primary hover:text-white px-3 py-2 w-full"
+        {!loading
+          ? data.map((item: IDoctor) => (
+              <div
+                key={item.id}
+                className="border hover:border-primary transition-all ease-in-out cursor-pointer rounded-xl p-3"
               >
-                Book Now
-              </Button>
-            </div>
-          </div>
-        ))}
+                <Image
+                  src={item.attributes.image.data.attributes.url}
+                  alt={item.attributes.Name}
+                  width={500}
+                  height={200}
+                  className="h-[200px] w-full object-cover rounded-lg "
+                />
+                <div className="mt-3 items-baseline gap-1 flex flex-col">
+                  <p className="text-[10px] bg-blue-100 p-1 rounded-full px-2 text-primary">
+                    {item.attributes.categories.data[0]?.attributes.Name}
+                  </p>
+                  <h2 className="font-bold">{item.attributes.Name}</h2>
+                  <p className="text-primary text-sm">
+                    {item.attributes.Year_of_Experience}
+                  </p>
+                  <p className="text-gray-500 text-sm">
+                    {item.attributes.Address}
+                  </p>
+                  <Button
+                    variant="outline"
+                    className="text-primary my-1.5 text-sm border-primary rounded-full hover:bg-primary hover:text-white px-3 py-2 w-full"
+                  >
+                    Book Now
+                  </Button>
+                </div>
+              </div>
+            ))
+          : Array.from(Array(8).keys()).map((item) => (
+              <div key={item} className="flex flex-col border border-gray-100 rounded-lg p-3 space-y-3">
+                <Skeleton className="h-[220px]" />
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-[250px]" />
+                  <Skeleton className="h-4 w-[250px]" />
+                  <Skeleton className="h-4 w-[250px]" />
+                  <Skeleton className="h-4 w-[200px]" />
+                </div>
+              </div>
+            ))}
       </div>
     </div>
   );
